4d0bd82e906e966caba6ea5a774348484d1d8b23
coco11563 <coco11563@yeah.net>
2017-04-20 16:05:10 +0800

    使用了一个fixednewthreadpool去加速数据导入 现在是一个八线程的导入程序 HTable变量使用ThreadLocal存储

--- src/main/java/HbaseImporter/HbaseImporter.java
+++ src/main/java/HbaseImporter/HbaseImporter.java
@@ -30,8 +30,12 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
+import static HbaseImporter.ImportThread.dateplus;
 import static HbaseImporter.ZipPart.GetFileStatus.showAllFiles;
 import static HbaseUtil.HbaseOperation.*;
 
@@ -39,8 +43,8 @@ public class HbaseImporter {
     private final static String cityNumPath = "./src/main/conf/cityNum.json";
     //2016-02-24 -->  2016-10-05
     private final static String timesetpath = "./src/main/conf/timeSetting.json";
-    private final static String completeddate = "./src/main/conf/CompletedDate.json";
-    private final static String completednum = "./src/main/conf/completednum.json";
+    public final static String completeddate = "./src/main/conf/CompletedDate.json";
+    public final static String completednum = "./src/main/conf/completednum.json";
 
     // 获取HBaseConfiguration
     public static Configuration cfg = HBaseConfiguration.create();
@@ -49,32 +53,14 @@ public class HbaseImporter {
     private static String TableName = "SinaWeiboDataStorage";
     //log4j initial
 
-    private static String cityTable = "weibodata.city_table";
-    private static String provinceTable = "weibodata.province_table";
-    private static String countryTable = "weibodata.country_table";
-    private static String checkinTable = "weibodata.check_in_table";
-    private static String timeTable = "weibodata.time_table";
-    private static String userTable = "weibodata.user_table";
+
 
     private static Logger logger = Logger.getLogger(HbaseImporter.class);
 
-    /**
-     *
-     * @param d 日期
-     * @param m 增加的天数
-     * @return 增加完天数后的日期
-     */
-    public static String dateplus(Date d , int m)
-    {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(d);
-        cal.add(Calendar.DATE, m);
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-        String returntype = df.format((cal.getTime()));
-        return returntype;
-    }
+
     @SuppressWarnings("deprecation")
     public static void main(String[] agrs) throws JSONException, ParseException, IOException {
+        ExecutorService es = Executors.newFixedThreadPool(8);
         // 获取城市的ID JSON文件
         JSONObject cityNumObject = JSONObject.fromObject(Read.readJson(cityNumPath));
         JSONObject timesetting = JSONObject.fromObject(Read.readJson(timesetpath));
@@ -85,148 +71,31 @@ public class HbaseImporter {
         int days = (int)((end.getTime() - start.getTime())/86400000) + 1;
         logger.debug("开始进行" + days+ "天的数据写入. . . ");
         // 开始进行hbase读写
-        JSONArray inputjson = new JSONArray();
         for(int iter = 0 ; iter < days ; iter ++)//天数
         {
-            int stornum = 0; //存储写入数量
-            long start_oneday_time = new Date().getTime();
+            AtomicInteger stornum = new AtomicInteger(0);
             String smbstring = "smb://biggrab:123456@192.168.1.111/biggrab/export/"+dateplus(start,iter)+"/";
             String smbzipstring = "smb://biggrab:123456@192.168.1.111/biggrab/export/"+dateplus(start,iter)+".zip";
             SmbFile fs = new SmbFile(smbstring);
-            System.out.println(smbstring);
+//            System.out.println(smbstring);
             if (!fs.exists()) {
                 continue;
             }
             List<String> filestatus = showAllFiles(fs);
-            SmbFile remotefs;
             for (String filestatu : filestatus) {//按城市遍历
 
-                if (filestatu.split("/").length == 8) {
-                    logger.debug("城市名:" + ((filestatu.split("/"))[7]).split("\\.")[0]);
-                    logger.debug("表名:" + TableName);
-                } else {
-                    logger.debug("城市名:" + ((filestatu.split("/"))[6]).split("\\.")[0]);
-                    logger.debug("表名:" + TableName);
-                }
+//                if (filestatu.split("/").length == 8) {
+//                    logger.debug("城市名:" + ((filestatu.split("/"))[7]).split("\\.")[0]);
+//                    logger.debug("表名:" + TableName);
+//                } else {
+//                    logger.debug("城市名:" + ((filestatu.split("/"))[6]).split("\\.")[0]);
+//                    logger.debug("表名:" + TableName);
+//                }
                 try {
-                    remotefs = new SmbFile(filestatu);
-                    remotefs.setConnectTimeout(600000);
-                    inputjson = Read.read_jsonFile(remotefs, "utf-8");
-                    stornum += inputjson.size();
-                    HTable _cityTable = new HTable(cfg, cityTable);
-                    HTable _provinceTable = new HTable(cfg, provinceTable);
-                    HTable _countryTable = new HTable(cfg, countryTable);
-                    HTable _checkinTable = new HTable(cfg, checkinTable);
-                    HTable _timeTable = new HTable(cfg, timeTable);
-                    HTable _userTable = new HTable(cfg, userTable);
-                    ArrayList<Put> putCityList = new ArrayList<>();
-                    ArrayList<Put> putProvinceList = new ArrayList<>();
-                    ArrayList<Put> putCountryList = new ArrayList<>();
-                    ArrayList<Put> putTimeList = new ArrayList<>();
-                    ArrayList<Put> putUserList = new ArrayList<>();
-                    ArrayList<Put> putCheckInList = new ArrayList<>();
-                    Time time;
-                    City city;
-                    Country country;
-                    Province province;
-                    User user;
-                    CheckIn checkIn;
-                    Put ptime;
-                    Put pcity;
-                    Put pprovince;
-                    Put pcountry;
-                    Put puser;
-                    Put pcheckin;
-                    for (int rownum = 0; rownum < inputjson.size(); rownum++)//按行数遍历
-                    {
-                        JSONObject jcell = inputjson.getJSONObject(rownum);
-                        HbaseCeller hbaseCeller = new HbaseCeller(jcell);
-
-                        rowKey rowKey = hbaseCeller.getRowKey();
-                        OtherInform otherInform = hbaseCeller.getOtherInform();
-                        //分装Celler
-                        time = new Time(rowKey.dateFormat.toDate());
-                        city = new City(rowKey.city_name, rowKey.city_id);
-                        province = new Province(rowKey.province_name, rowKey.province_id);
-                        country = new Country(rowKey.country_name, rowKey.country_id);
-                        user = new User(rowKey.user_id, otherInform.getGender(), otherInform.getUsername());
-                        checkIn = new CheckIn(rowKey.weibo_id, rowKey.geo_Hash,
-                                otherInform.getContent(), jcell.toString(),
-                                city, province, country, time, user, rowKey.dateFormat.toDate(), otherInform.getPicURL());
-                        //插入流写入
-                        ptime = putTime(time);
-                        pcity = putCity(city);
-                        pprovince = putProvince(province);
-                        pcountry = putCountry(country);
-                        puser = putUser(user);
-                        pcheckin = putCheckIn(checkIn);
-                        putTimeList.add(ptime);
-                        putCheckInList.add(pcheckin);
-                        putUserList.add(puser);
-                        putCountryList.add(pcountry);
-                        putProvinceList.add(pprovince);
-                        putCityList.add(pcity);
-                        if (putCheckInList.size() > 1000) {
-                            _cityTable.put(putCityList);
-                            _checkinTable.put(putCheckInList);
-                            _countryTable.put(putCountryList);
-                            _provinceTable.put(putProvinceList);
-                            _timeTable.put(putTimeList);
-                            _userTable.put(putUserList);
-                            _cityTable.flushCommits();
-                            _checkinTable.flushCommits();
-                            _countryTable.flushCommits();
-                            _provinceTable.flushCommits();
-                            _timeTable.flushCommits();
-                            _userTable.flushCommits();
-                            putCheckInList.clear();
-                            putCityList.clear();
-                            putCountryList.clear();
-                            putProvinceList.clear();
-                            putTimeList.clear();
-                            putUserList.clear();
-                            logger.debug("进行一次写入");
-                        }
-                    }
-                    _cityTable.put(putCityList);
-                    _checkinTable.put(putCheckInList);
-                    _countryTable.put(putCountryList);
-                    _provinceTable.put(putProvinceList);
-                    _timeTable.put(putTimeList);
-                    _userTable.put(putUserList);
-                    _cityTable.flushCommits();
-                    _checkinTable.flushCommits();
-                    _countryTable.flushCommits();
-                    _provinceTable.flushCommits();
-                    _timeTable.flushCommits();
-                    _userTable.flushCommits();
-                    putCheckInList.clear();
-                    putCityList.clear();
-                    putCountryList.clear();
-                    putProvinceList.clear();
-                    putTimeList.clear();
-                    putUserList.clear();
-                    logger.debug("结尾处进行一次写入");
-                    _cityTable.close();
-                    _provinceTable.close();
-                    _checkinTable.close();
-                    _countryTable.close();
-                    _timeTable.close();
-                    _userTable.close();
+                    es.submit(new ImportThread(filestatu));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-            }
-            //完成一天的写入
-            String times = Integer.toString(iter);
-            long end_oneday_time = new Date().getTime();
-            long use_time = (end_oneday_time - start_oneday_time)/(1000*60);//分钟存储运行时间
-            try {
-                numInsert(dateplus(start,iter),stornum) ;
-                dateInsert((times+"_"+use_time), dateplus(start,iter));
-            } catch (Exception e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
             }
 //            //开始压缩作业
 //            logger.info("start to zip the file");
@@ -234,38 +103,9 @@ public class HbaseImporter {
 //            logger.info("zip over");
 //            fs.delete();
         }
-    }
-
-    @SuppressWarnings("unused")
-    private static void dateInsert(String iter,String date) throws JSONException, IOException {
-        // TODO Auto-generated method stub
-        JSONObject cd = new JSONObject();
-        cd.element("completedate_" + iter,date );
-        File f = new File(completeddate);
-        if(!f.exists()){
-            f.createNewFile();
+        while (es.isTerminated()){
+            es.shutdown();
         }
-        FileWriter fileWritter = new FileWriter(f.getName(),true);
-        BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
-        bufferWritter.write(cd.toString());
-        bufferWritter.close();
-
-        logger.info("Done One day");
-    }
-    private static void numInsert(String date,int stornum) throws JSONException, IOException {
-        // TODO Auto-generated method stub
-        JSONObject cd = new JSONObject();
-        cd.element(date,stornum );
-        File f = new File(completednum);
-        if(!f.exists()){
-            f.createNewFile();
-        }
-        FileWriter fileWritter = new FileWriter(f.getName(),true);
-        BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
-        bufferWritter.write(cd.toString());
-        bufferWritter.close();
-
-        logger.info("log one day num");
     }
     private static String getUserID(String JsonString) throws JSONException {
         // TODO Auto-generated method stub
@@ -275,4 +115,5 @@ public class HbaseImporter {
         String renturnstring = gender + "_" +  id;
         return renturnstring;
     }
+
 }
---
+++ src/main/java/HbaseImporter/ImportThread.java
@@ -0,0 +1,319 @@
+package HbaseImporter;
+
+import StarModelBuilder.CheckIn;
+import StarModelBuilder.Location.City;
+import StarModelBuilder.Location.Country;
+import StarModelBuilder.Location.Province;
+import StarModelBuilder.Time.Time;
+import StarModelBuilder.User.User;
+import datastruct.KeySizeException;
+import jcifs.smb.SmbFile;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONException;
+import net.sf.json.JSONObject;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.log4j.Logger;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import static HbaseImporter.HbaseImporter.cfg;
+import static HbaseImporter.HbaseImporter.completeddate;
+import static HbaseImporter.HbaseImporter.completednum;
+import static HbaseUtil.HbaseOperation.*;
+import static HbaseUtil.HbaseOperation.putCheckIn;
+import static HbaseUtil.HbaseOperation.putUser;
+
+/**
+ * Created by Administrator on 2017/4/19.
+ *
+ */
+public class ImportThread implements Runnable {
+    private static String cityTable = "weibodata.city_table";
+    private static String provinceTable = "weibodata.province_table";
+    private static String countryTable = "weibodata.country_table";
+    private static String checkinTable = "weibodata.check_in_table";
+    private static String timeTable = "weibodata.time_table";
+    private static String userTable = "weibodata.user_table";
+    private static Logger logger = Logger.getLogger(ImportThread.class);
+    private HTable _cityTable = null;
+    private HTable _provinceTable  = null;
+    private HTable _countryTable = null;
+    private HTable _checkinTable = null;
+    private HTable _timeTable = null;
+    private HTable _userTable = null;
+    private String filestatu;
+    private int iter;
+    ImportThread(String filestatu) {
+        this.filestatu = filestatu;
+        _cityTable = inialCityTable();
+        _provinceTable = inialProvinceTable();
+        _countryTable = inialCountryTable();
+        _checkinTable = inialWeiboTable();
+        _timeTable = inialTimeTable();
+        _userTable = inialUserTable();
+    }
+    public final static ThreadLocal<HTable> cityTableLocal = new ThreadLocal<>();
+    public final static ThreadLocal<HTable> provinceTableLocal = new ThreadLocal<>();
+    public final static ThreadLocal<HTable> countryTableLocal = new ThreadLocal<>();
+    public final static ThreadLocal<HTable> checkinTableLocal = new ThreadLocal<>();
+    public final static ThreadLocal<HTable> timeTableLocal = new ThreadLocal<>();
+    public final static ThreadLocal<HTable> userTableLocal = new ThreadLocal<>();
+    @Override
+    public void run() {
+//        String times = Integer.toString(iter);
+//        long end_oneday_time = new Date().getTime();
+//        long use_time = (end_oneday_time - start_oneday_time)/(1000*60);//分钟存储运行时间
+//        try {
+//            numInsert(dateplus(start,iter),stornum) ;
+//            dateInsert((times+"_"+use_time), dateplus(start,iter));
+//        } catch (Exception e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+        Import(filestatu);
+    }
+    @SuppressWarnings("unused")
+    private static void dateInsert(String iter,String date) throws JSONException, IOException {
+        // TODO Auto-generated method stub
+        JSONObject cd = new JSONObject();
+        cd.element("completedate_" + iter,date );
+        File f = new File(completeddate);
+        if(!f.exists()){
+            f.createNewFile();
+        }
+        FileWriter fileWritter = new FileWriter(f.getName(),true);
+        BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
+        bufferWritter.write(cd.toString());
+        bufferWritter.close();
+
+        logger.info("Done One day");
+    }
+    private static void numInsert(String date,int stornum) throws JSONException, IOException {
+        // TODO Auto-generated method stub
+        JSONObject cd = new JSONObject();
+        cd.element(date,stornum );
+        File f = new File(completednum);
+        if(!f.exists()){
+            f.createNewFile();
+        }
+        FileWriter fileWritter = new FileWriter(f.getName(),true);
+        BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
+        bufferWritter.write(cd.toString());
+        bufferWritter.close();
+
+        logger.info("log one day num");
+    }
+
+    /**
+     *
+     * @param d 日期
+     * @param m 增加的天数
+     * @return 增加完天数后的日期
+     */
+    static String dateplus(Date d, int m)
+    {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(d);
+        cal.add(Calendar.DATE, m);
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        return df.format((cal.getTime()));
+    }
+    private void Import(String filestatu) {
+        SmbFile remotefs = null;
+        try {
+            remotefs = new SmbFile(filestatu);
+            remotefs.setConnectTimeout(600000);
+        } catch (MalformedURLException e) {
+            logger.error("smb出现问题");
+            e.printStackTrace();
+        }
+        long start_oneday_time = new Date().getTime();
+        JSONArray inputjson = null;
+        try {
+            inputjson = Read.read_jsonFile(remotefs, "utf-8");
+//            stornum .addAndGet( inputjson.size() );
+            ArrayList<Put> putCityList = new ArrayList<>();
+            ArrayList<Put> putProvinceList = new ArrayList<>();
+            ArrayList<Put> putCountryList = new ArrayList<>();
+            ArrayList<Put> putTimeList = new ArrayList<>();
+            ArrayList<Put> putUserList = new ArrayList<>();
+            ArrayList<Put> putCheckInList = new ArrayList<>();
+            Time time;
+            City city;
+            Country country;
+            Province province;
+            User user;
+            CheckIn checkIn;
+            Put ptime;
+            Put pcity;
+            Put pprovince;
+            Put pcountry;
+            Put puser;
+            Put pcheckin;
+            for (int rownum = 0; rownum < inputjson.size(); rownum++)//按行数遍历
+            {
+                JSONObject jcell = inputjson.getJSONObject(rownum);
+                HbaseCeller hbaseCeller = new HbaseCeller(jcell);
+                rowKey rowKey = hbaseCeller.getRowKey();
+                OtherInform otherInform = hbaseCeller.getOtherInform();
+                //分装Celler
+                time = new Time(rowKey.dateFormat.toDate());
+                city = new City(rowKey.city_name, rowKey.city_id);
+                province = new Province(rowKey.province_name, rowKey.province_id);
+                country = new Country(rowKey.country_name, rowKey.country_id);
+                user = new User(rowKey.user_id, otherInform.getGender(), otherInform.getUsername());
+                checkIn = new CheckIn(rowKey.weibo_id, rowKey.geo_Hash,
+                        otherInform.getContent(), jcell.toString(),
+                        city, province, country, time, user, rowKey.dateFormat.toDate(), otherInform.getPicURL());
+                //插入流写入
+                ptime = putTime(time);
+                pcity = putCity(city);
+                pprovince = putProvince(province);
+                pcountry = putCountry(country);
+                puser = putUser(user);
+                pcheckin = putCheckIn(checkIn);
+                putTimeList.add(ptime);
+                putCheckInList.add(pcheckin);
+                putUserList.add(puser);
+                putCountryList.add(pcountry);
+                putProvinceList.add(pprovince);
+                putCityList.add(pcity);
+                if (putCheckInList.size() > 1000) {
+                    _cityTable.put(putCityList);
+                    _checkinTable.put(putCheckInList);
+                    _countryTable.put(putCountryList);
+                    _provinceTable.put(putProvinceList);
+                    _timeTable.put(putTimeList);
+                    _userTable.put(putUserList);
+                    _cityTable.flushCommits();
+                    _checkinTable.flushCommits();
+                    _countryTable.flushCommits();
+                    _provinceTable.flushCommits();
+                    _timeTable.flushCommits();
+                    _userTable.flushCommits();
+                    putCheckInList.clear();
+                    putCityList.clear();
+                    putCountryList.clear();
+                    putProvinceList.clear();
+                    putTimeList.clear();
+                    putUserList.clear();
+                    logger.debug("进行一次写入");
+                }
+            }
+            _cityTable.put(putCityList);
+            _checkinTable.put(putCheckInList);
+            _countryTable.put(putCountryList);
+            _provinceTable.put(putProvinceList);
+            _timeTable.put(putTimeList);
+            _userTable.put(putUserList);
+            _cityTable.flushCommits();
+            _checkinTable.flushCommits();
+            _countryTable.flushCommits();
+            _provinceTable.flushCommits();
+            _timeTable.flushCommits();
+            _userTable.flushCommits();
+            putCheckInList.clear();
+            putCityList.clear();
+            putCountryList.clear();
+            putProvinceList.clear();
+            putTimeList.clear();
+            putUserList.clear();
+            logger.debug("结尾处进行一次写入");
+//            _cityTable.close();
+//            _provinceTable.close();
+//            _checkinTable.close();
+//            _countryTable.close();
+//            _timeTable.close();
+//            _userTable.close();
+            long end_oneday_time = new Date().getTime();
+            logger.info("该城市使用了" + (end_oneday_time - start_oneday_time) / 1000 + "秒");
+        } catch (ParseException | KeySizeException | IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+
+    private static HTable inialCityTable() {
+        HTable cityTable = cityTableLocal.get();
+        if (cityTable == null) {
+            try {
+                cityTable = new HTable(cfg, ImportThread.cityTable);
+                cityTableLocal.set(cityTable);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return cityTable;
+    }
+    private static HTable inialProvinceTable() {
+        HTable provincetable = provinceTableLocal.get();
+        if (provincetable == null) {
+            try {
+                provincetable = new HTable(cfg, ImportThread.provinceTable);
+                provinceTableLocal.set(provincetable);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return provincetable;
+    }
+    private static HTable inialCountryTable() {
+        HTable countryTable = countryTableLocal.get();
+        if (countryTable == null) {
+            try {
+                countryTable = new HTable(cfg, ImportThread.countryTable);
+                countryTableLocal.set(countryTable);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return countryTable;
+    }
+    private static HTable inialTimeTable() {
+        HTable timeTable = timeTableLocal.get();
+        if (timeTable == null) {
+            try {
+                timeTable = new HTable(cfg, ImportThread.timeTable);
+                timeTableLocal.set(timeTable);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return timeTable;
+    }
+    private static HTable inialUserTable() {
+        HTable userTable = userTableLocal.get();
+        if (userTable == null) {
+            try {
+                userTable = new HTable(cfg, ImportThread.userTable);
+                userTableLocal.set(userTable);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return userTable;
+    }
+
+    private static HTable inialWeiboTable() {
+        HTable weiboTable = checkinTableLocal.get();
+        if (weiboTable == null) {
+            try {
+                weiboTable = new HTable(cfg, ImportThread.checkinTable);
+                checkinTableLocal.set(weiboTable);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return weiboTable;
+    }
+}
